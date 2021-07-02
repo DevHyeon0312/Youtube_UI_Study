a93cabfdec80fa9b3688eacedd2bbc3fc7d1fc8d
devhyeon <devhyeon0312@gmail.com>
2021-07-02 11:30:43 +0900

    dp.kt 주석

--- app/src/main/java/com/devhyeon/youtubelayout/utils/dp.kt
+++ app/src/main/java/com/devhyeon/youtubelayout/utils/dp.kt
@@ -2,8 +2,10 @@ package com.devhyeon.youtubelayout.utils
 
 import android.content.res.Resources
 
+/** Int Value -> dp */
 val Int.dp: Int
     get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
 
+/** Float Value -> dp */
 val Float.dp: Int
     get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
